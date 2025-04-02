7313a982823145ad13ccc8d9f06bd8bca9952955
ArsenZar <arsenzar@gmail.com>
2025-04-02 18:26:26 +0300

    practice-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,16 @@
-console.log("200");
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  isPublic: true,
+  rating: 8.38,
+};
+
+// Деструктуризуємо
+const { title, a, isP, r } = book;
+
+// Використовуємо
+const accessType = isP ? "pulbic" : "private";
+const message = `Book ${title} by author ${a} with rating ${r} is in ${accessType} access!`;
+
+console.log(message);
